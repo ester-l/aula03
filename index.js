4a761bc8d2c080ffedcae3afcd8576716150820c
ester-l <ester.limabar@gmail.com>
2026-03-06 09:33:32 -0300

    “primeiro”

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+app.get("/idade/:nome/:idade", (req, res)=>{
+    const {nome, idade} = req.params
+    res.status (200).json({
+        mensagem:`Nome: ${nome}, Idade: ${idade}`
+    })
+})
+
+app.listen(3014, ()=>{
+    console.log("Ester o servidor esta Ligado")
+})
